e8100c0f20194098e8db4823708107bdfc9278de
Brendan <brendanlawless@hotmail.com>
2024-12-14 21:44:35 +0000

    chore: add body parser to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
 import cors from "cors"
 import routes from "./routes/index.ts"
 import "../config/passport.ts";
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3000;
 const app: Express = express();
 
 app.use(cors())
+app.use(bodyParser.json())
 
 app.use("/post", routes.post)
 app.use("/register", routes.register)
