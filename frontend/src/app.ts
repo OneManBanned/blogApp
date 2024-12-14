fd5712910230c7fc42ecca63dac0e31cf5b38e65
Brendan <brendanlawless@hotmail.com>
2024-12-14 21:45:34 +0000

    feat: submit form to backend api

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,14 +1,16 @@
 import express, {Request, Response} from "express"
-import ejs from "ejs"
+import path from "path";
+import ejs from "ejs";
 
 const PORT = process.env.PORT || 5000
 const app = express();
 
+app.set('views', path.join(__dirname, "../views"))
 app.engine("html", ejs.renderFile);
 app.set('view engine', "ejs")
 
 app.get("/", (req: Request, res: Response) => {
-    res.render('index.html')
+    res.render('index.html');
 })
 
 app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`))
