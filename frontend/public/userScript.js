08f25bac89f23ccc8493048d281b06d608ec9b9a
Brendan <brendanlawless@hotmail.com>
2024-12-16 16:01:24 +0000

    feat[register]: add frontend registration
    - add registration route and html
    - add script to make POST request to backend
    - add head template
    - add static files folder
    - add login route for redirect after register success

---
+++ frontend/public/userScript.js
@@ -0,0 +1,55 @@
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll("input");
+
+form.addEventListener("submit", handleFormSubmit);
+
+async function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const url = form.action;
+
+    try {
+        const formData = new FormData(form);
+        const responseData = await postFormDataAsJson({ url, formData });
+
+        responseData.success
+            ? (location.href = "http://localhost:5000/login")
+            : displayServerErrors(responseData, inputs);
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function postFormDataAsJson({ url, formData }) {
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formDataJsonString = JSON.stringify(plainFormData);
+
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: formDataJsonString,
+    };
+
+    const response = await fetch(url, fetchOptions);
+
+    if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+    }
+
+    return response.json();
+}
+
+function displayServerErrors(errors, inputs) {
+    for (let i = 0; i < inputs.length; i++) {
+        let input = inputs[i];
+        let span = input.nextElementSibling;
+        input.name in errors
+            ? (span.innerHTML = errors[input.name])
+            : (span.innerHTML = "");
+    }
+}
--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -5,13 +5,22 @@ import ejs from "ejs";
 const PORT = process.env.PORT || 5000
 const app = express();
 
+app.use(express.static(path.join(__dirname, "../public")))
+
 app.set('views', path.join(__dirname, "../views"))
 app.engine("html", ejs.renderFile);
 app.set('view engine', "ejs")
 
+app.get("/register", (req: Request, res: Response) => {
+    res.render('register.html');
+})
+app.get("/login", (req: Request, res: Response) => {
+    res.render('login.html');
+})
 app.get("/", (req: Request, res: Response) => {
     res.render('index.html');
 })
+
 
 app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`))
 
