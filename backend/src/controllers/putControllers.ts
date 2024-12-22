44ca757592d4e491d7e4462adc5cf03c131c62f2
Brendan <brendanlawless@hotmail.com>
2024-12-22 13:38:27 +0000

    feat[put]: handle post put requests on the backend

--- backend/src/controllers/putControllers.ts
+++ backend/src/controllers/putControllers.ts
@@ -1,14 +1,51 @@
+import asyncHandler from "express-async-handler";
+import prisma from "../../config/prismaClient";
+import { body, validationResult } from "express-validator";
 import { Request, Response } from "express";
+import { createErrorsMap } from "../../utils/createErrMap";
+
+const validatePost = [
+    body("title")
+        .notEmpty()
+        .withMessage("required")
+        .isLength({ max: 256 })
+        .withMessage("title is too long"),
+    body("content").notEmpty().withMessage("required"),
+];
 
 const put = {
-    
-    post: (req: Request, res: Response) => {
+    post: [
+        ...validatePost,
+        asyncHandler(async (req: Request, res: Response) => {
 
-    },
+            const valid = validationResult(req)
 
-    comment: (req: Request, res: Response) => {
+            if (!valid.isEmpty()) {
+                const errors = createErrorsMap(valid.array({ onlyFirstError: true }))
+                res.json(errors)
+            }
 
-    }
-}
+            const { title, content, publish } = req.body;
+            const { postId } = req.params;
+            const published = publish == "on" ? true : false;
+
+            await prisma.post.update({
+                where: {
+                    id: +postId,
+                },
+                data: {
+                    title,
+                    content,
+                    published,
+                },
+            });
+
+
+            res.json({ success: true });
+        }),
+    ],
+
+    comment: (req: Request, res: Response) => { },
+};
 
 export default put;
