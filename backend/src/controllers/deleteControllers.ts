cb143bc067d99473df3a3165bab5272deb996cef
Brendan <brendanlawless@hotmail.com>
2024-12-24 10:30:38 +0000

    feat[delete]: add delete post logic to restful api

--- backend/src/controllers/deleteControllers.ts
+++ backend/src/controllers/deleteControllers.ts
@@ -1,10 +1,22 @@
+import asyncHandler from "express-async-handler";
 import {Request, Response} from "express"
+import prisma from "../../config/prismaClient";
 
 const del = {
 
-    post: (req: Request, res: Response) => {
+    post: asyncHandler( async (req: Request, res: Response) => {
 
-    },
+        const { postId } = req.params
+
+        await prisma.post.delete({
+            where: {
+                id: +postId
+            }
+        })
+                
+        res.json({success: true})
+
+    }),
 
     comment: (req: Request, res: Response) => {
 
