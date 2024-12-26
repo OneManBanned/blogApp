ddc33dcf94a4516845b46e47d39d2a4afd145e20
Brendan <brendanlawless@hotmail.com>
2024-12-26 12:01:54 +0000

    fix: update page after comment add

--- backend/src/controllers/postControllers.ts
+++ backend/src/controllers/postControllers.ts
@@ -6,66 +6,66 @@ import prisma from "../../config/prismaClient";
 import { currentUser } from "../../utils/adminAuth";
 
 interface PostInput {
-  title: string;
-  content: string;
-  publish: string;
+    title: string;
+    content: string;
+    publish: string;
 }
 
 const validatePost = [
-  body("title")
-    .notEmpty()
-    .withMessage("required")
-    .isLength({ max: 256 })
-    .withMessage("title is too long"),
-  body("content").notEmpty().withMessage("required"),
+    body("title")
+        .notEmpty()
+        .withMessage("required")
+        .isLength({ max: 256 })
+        .withMessage("title is too long"),
+    body("content").notEmpty().withMessage("required"),
 ];
 
 const validateComment = [body("comment").notEmpty().withMessage("required")];
 
 const post = {
-  post: [
-    ...validatePost,
-    asyncHandler(async (req: Request, res: Response) => {
-      const valid = validationResult(req);
-      if (!valid.isEmpty()) {
-        const errors = createErrorsMap(valid.array({ onlyFirstError: true }));
-        res.json(errors);
-      } else {
-        const { title, content, publish } = req.body as PostInput;
-        const { id: authorId } = req.user as currentUser;
+    post: [
+        ...validatePost,
+        asyncHandler(async (req: Request, res: Response) => {
+            const valid = validationResult(req);
+            if (!valid.isEmpty()) {
+                const errors = createErrorsMap(valid.array({ onlyFirstError: true }));
+                res.json(errors);
+            } else {
+                const { title, content, publish } = req.body as PostInput;
+                const { id: authorId } = req.user as currentUser;
 
-        const published = publish == "on" ? true : false;
-        await prisma.post.create({
-          data: { title, content, published, authorId },
-        });
-        res.json({ success: true });
-      }
-    }),
-  ],
+                const published = publish == "on" ? true : false;
+                await prisma.post.create({
+                    data: { title, content, published, authorId },
+                });
+                res.json({ success: true });
+            }
+        }),
+    ],
 
-  comment: [
-    ...validateComment,
-    asyncHandler(async (req: Request, res: Response) => {
+    comment: [
+        ...validateComment,
+        asyncHandler(async (req: Request, res: Response) => {
+            const valid = validationResult(req);
 
-      const valid = validationResult(req);
-
-      if (!valid.isEmpty()) {
-        const errors = createErrorsMap(valid.array({ onlyFirstError: true }));
-        res.json(errors);
-      } else {
-        const { comment } = req.body;
-        const { postId } = req.params;
-        const { id } = req.user as currentUser;
-        await prisma.comment.create({
-          data: {
-            postId: +postId,
-            authorId: id,
-            content: comment,
-          },
-        });
-      }
-    }),
-  ],
+            if (!valid.isEmpty()) {
+                const errors = createErrorsMap(valid.array({ onlyFirstError: true }));
+                res.json(errors);
+            } else {
+                const { comment } = req.body;
+                const { postId } = req.params;
+                const { id } = req.user as currentUser;
+                await prisma.comment.create({
+                    data: {
+                        postId: +postId,
+                        authorId: id,
+                        content: comment,
+                    },
+                });
+                res.json({success: true})
+            }
+        }),
+    ],
 };
 
 export default post;
