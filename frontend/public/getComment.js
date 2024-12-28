99908546ba18db5ddcafd4be4a8763d51c301185
Brendan <brendanlawless@hotmail.com>
2024-12-28 10:55:08 +0000

    feat[comment]: populate edit form with comment values

--- frontend/public/getComment.js
+++ frontend/public/getComment.js
@@ -1,35 +1,41 @@
-const container = document.querySelector("#container");
 window.addEventListener("load", displayComment);
 
-
 async function displayComment() {
-    console.log(location.pathname)
-    if (token) {
-        const responseData = await fetchPosts(
-            token,
-            `http://localhost:3000/post${location.pathname}`,
-        );
-        display(responseData);
-    } else {
-        location.href = "http://localhost:5000/";
-    }
+  if (token) {
+    const responseData = await fetchPosts(
+      token,
+      `http://localhost:3000/post${location.pathname}`,
+    );
+    display(responseData);
+  } else {
+    location.href = "http://localhost:5000/";
+  }
 }
 
 async function fetchPosts(token, url) {
-    const fetchOptions = {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-        },
-    };
+  const fetchOptions = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    const response = await fetch(url, fetchOptions);
+  const response = await fetch(url, fetchOptions);
 
-    if (!response.ok) {
-        const errorMessage = await response.text();
-        throw new Error(errorMessage);
-    }
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
 
-    return response.json();
+  return response.json();
 }
+
+function display(data) {
+  const textarea = document.querySelector("#comment");
+  const { content } = data;
+
+    textarea.innerHTML = content
+
+}
+
